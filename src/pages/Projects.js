0c280433dda72275c350896876c8b739b2b10c1b
“Nahuel <ngomezdw@gmail.com>
2022-03-27 19:26:19 -0300

    New projects added

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -21,6 +21,54 @@ const Projects = () => {
       link: "https://coderhouse-react-six.vercel.app/",
       github: "https://github.com/almanoduerme/coderhouse-react/",
     },
+    {
+      title: "Form Validation",
+      description:
+        "Aplicación web creada con ReactJS. Se utiliza Formik con Yup para validar los campos.",
+      image: "/images/FormValidation.png",
+      link: "https://formwithformik.vercel.app/",
+      github: "https://github.com/almanoduerme/formwithformik",
+    },
+    {
+      title: "Calculator",
+      description:
+        "Aplicación web creada con JavaScript. Se utilizan funciones y operadores matemáticos.",
+      image: "/images/Calculator.png",
+      link: "https://calculator-roan-pi.vercel.app/",
+      github: "https://github.com/almanoduerme/calculator",
+    },
+    {
+      title: "Tuitah",
+      description:
+        "Aplicación web creada con JavaScript. Simula una interacción de Twitter (similar a un To-Do List).",
+      image: "/images/Tuitah.png",
+      link: "https://almanoduerme.github.io/proyectofinalcoderhouse/",
+      github: "https://github.com/almanoduerme/proyectofinalcoderhouse",
+    },
+    {
+      title: "La Pucha Design",
+      description:
+        "Sitio web estático creado con HTML, CSS y JavaScript. El mismo fue un proyecto pago por CoderHouse.",
+      image: "/images/LaPuchaDesign.png",
+      link: "https://almanoduerme.github.io/lapuchadesign/",
+      github: "https://github.com/almanoduerme/lapuchadesign",
+    },
+    {
+      title: "Valorant Random Map",
+      description:
+        "Esta aplicación fue desarrollada con JavaScript. Se simula la API de Valorant para obtener un mapa aleatorio.",
+      image: "/images/ValorantRandomMap.png",
+      link: "https://almanoduerme.github.io/randommap/",
+      github: "https://github.com/almanoduerme/randommap",
+    },
+    {
+      title: "Chocodulce;",
+      description:
+        "Este proyecto fue la entrega final de 'Desarrollo Web' dictado en CoderHouse.",
+      image: "/images/Chocodulce.png",
+      link: "https://almanoduerme.github.io/chocodulce/",
+      github: "https://github.com/almanoduerme/chocodulce",
+    },
   ];
 
   // Loading spinner.
@@ -63,11 +111,19 @@ const Projects = () => {
                   <p>{project.description}</p>
                 </div>
                 <div className="project-links">
-                  <a className="project-link" href={project.link}>
-                    LIVE
-                  </a>
-                  <a className="project-link" href={project.github}>
+                  <a
+                    target="blank"
+                    className="project-link"
+                    href={project.github}
+                  >
                     GITHUB
+                  </a>
+                  <a
+                    target="blank"
+                    className="project-link"
+                    href={project.link}
+                  >
+                    LIVE
                   </a>
                 </div>
               </div>
